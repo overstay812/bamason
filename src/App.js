7c952823bea7dbdd318f1b091c5127217da33e40
overstay812 <overstay812@gmail.com>
2020-11-06 13:44:10 +0300

    added login page styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import styles from './App.module.css'
 import Header from './conponents/Header'
+import Login from './conponents/login/Login'
 function App() {
   return (
     <div className={styles.container}>
       <Header />
+      <Login/>
     </div>
   );
 }
---
+++ src/conponents/login/Login.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import styles from "./Login.module.css";
+const Login = () => {
+  return (
+    <div className={styles.login}>
+      <div className={styles.container}>
+        <form className={styles.form}>
+          <label htmlFor="name">Введите имя </label>
+          <input type="text" id="name" className={styles.name} />
+          <label htmlFor="passowd">Введите пароль </label>
+          <input type="text" id="passowd" />
+          <button type="button" className={styles.button}>
+            Войти
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
